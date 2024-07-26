8c66d4915250575b0c0abaef1af29565f2247bd2
Tarunsai97 <62532509+Tarunsai97@users.noreply.github.com>
2024-07-26 08:27:08 +0530

    Update primeNumbers.cs

--- Loops/primeNumbers.cs
+++ Loops/primeNumbers.cs
@@ -1,1 +1,31 @@
+using System;
 
+namespace Loops
+{
+    class Loop
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number to print the primne numbers");
+            int num = Convert.ToInt32(Console.ReadLine());
+        
+            for(int i = 1; i <= num; i++)
+            {
+                int count = 0;
+                for(int j=1; j<= num; j++)
+                {
+                    if(i%j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if(count == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
