ec34680eea5701dc713ecb9c8107e708b1b0fb82
Tarunsai97 <62532509+Tarunsai97@users.noreply.github.com>
2024-07-26 08:40:42 +0530

    Update factorial.cs

--- Loops/factorial.cs
+++ Loops/factorial.cs
@@ -1,1 +1,23 @@
+using System;
 
+
+namespace Loops
+{
+    class Loop
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number know the factorial : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+
+            for(int i = num; i > 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.Write(fact);
+
+            Console.ReadLine();
+        }
+    }
+}
