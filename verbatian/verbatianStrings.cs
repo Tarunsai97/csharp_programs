fd0aad4bd6ebc024e6729f3831554457c2f946b6
Tarunsai97 <62532509+Tarunsai97@users.noreply.github.com>
2024-07-27 14:49:01 +0530

    Create verbatianStrings.cs

---
+++ verbatian/verbatianStrings.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace Verbatian{
+    class VBString{
+        public static void Main(string[] args){
+            string path = "c:\\Tarun\\sai";
+            Console.WriteLine(path);
+            path = @"c:\Tarun\sai";
+            Console.WriteLine(path);
+        }
+    }
+}
