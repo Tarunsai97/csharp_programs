3c4c726529f25254698bd3f0db0e0327217f39f7
Tarunsai97 <62532509+Tarunsai97@users.noreply.github.com>
2024-07-28 09:41:48 +0530

    Create minMaxArray.cs

---
+++ arrays/minMaxArray.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ArrayOp{
+    class ArrayFun{
+      public static void Main(string[] args){
+        Console.WriteLine("Enter how many numbers you want to insert");
+        var indexLength = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[indexLength];
+        for(int i=0; i<=numbers.Length-1;i++){
+            numbers[i]=Convert.ToInt32(Console.ReadLine());
+        }
+        Array.Sort(numbers);
+        int min = numbers[0];
+        int max = numbers[indexLength-1];
+        Console.WriteLine($"{min}");
+        Console.WriteLine($"{max}");
+      }
+    }
+}
