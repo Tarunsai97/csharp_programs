df6505282ed507a6e6d6d4f4eae3fb38135c0fe4
Tarunsai97 <62532509+Tarunsai97@users.noreply.github.com>
2024-07-28 11:51:25 +0530

    Create basicFunc.cs

---
+++ Functions/basicFunc.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Functions{
+    class BasicFun{
+        public static void Main(string[] args){
+            Welcome();
+        }
+        
+        public static void Welcome(){
+            Console.WriteLine("Welcome To New World !!");
+        }
+    }
+}
